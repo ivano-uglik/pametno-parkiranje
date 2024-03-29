0aca68ad7e6c4bb96c218ffa7ea42a66fb395612
ivano-uglik <ivano.uglik7@gmail.com>
2024-03-29 17:22:14 +0100

    added Leaflet map library

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,7 @@ import {
   IonContent,
   IonSearchbar,
 } from '@ionic/angular/standalone';
-
+import * as L from 'leaflet';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -15,5 +15,12 @@ import {
   imports: [IonSearchbar, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class Tab1Page {
-  constructor() {}
+  private map!: L.Map;
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([45.55742, 18.68733], 20);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Koristi OpenStreetMap',
+    }).addTo(this.map);
+  }
 }
